eae2de741454556d18eaef752abf102e2ea83de2
Stevenkb42 <53572564+Stevenkb42@users.noreply.github.com>
2019-10-13 21:53:44 -0600

    Add files via upload

---
+++ MigrationTool.ps1
@@ -0,0 +1,208 @@
+# Title: MigrationTool.ps1
+# Authors:
+# Steven Barnes
+# Purpose: 
+# Remotely Migrate a user's data after a profile rebuild.
+# Version: 1.00
+
+# Create bat file to migrate data
+
+param(
+    [Parameter(Mandatory=$True,Position=1)]
+    $Machine, $User, $Date, $LongDate, $ProfileLog
+)
+
+$Host.UI.RawUI.WindowTitle = "Profile Migration tool"
+$ErrorActionPreference = 'SilentlyContinue'
+Clear-Host
+
+#*****Starting Fix*****
+Write-Host ""
+Write-Warning "DO NOT Close Window Until the Scrpt is Finished"
+Write-Host ""
+
+Write-Host "Migrating $User's profile data on $machine" -ForegroundColor Magenta
+Write-Host ""
+
+Write-Host ""
+Write-Warning "DO NOT Close Window Until the Scrpt is Finished"
+Write-Host ""
+Pause
+$LongDate = (Get-Date).ToString("yyyy.MM.dd_HHmm")
+
+New-Item "C:\AIP\Paths.txt" -ItemType file | Out-Null
+$RebuildLog = "C:\AIP\Paths.txt"
+
+$OldProfile = 'old.' + $User + '.' + $Date
+
+$Agent = ($env:USERNAME)
+$error.clear() 
+$ErrorFile = "\\ADIDBO390422\C$\AIP\Migration\$machine-MigrateErrorLog-$LongDate.txt"
+Add-Content -Path $ErrorFile "`r`nFix ran by $Agent on $LongDate"
+
+Write-Host ""
+Write-Host "Starting migration" -ForegroundColor Yellow
+
+# Importing NTUSER.DAT
+Write-Host "Importing registry hive"
+Robocopy.exe "\\$machine\C$\Users\$OldProfile" "C:\AIP" NTUSER.DAT `/MT:256 | Out-Null
+$NTUserImp = (Test-Path 'C:\AIP\NTUSER.DAT')
+If ($NTUserImp -eq $False){
+    Write-Host "Unable to copy NTUSER.DAT, trying again"
+    Robocopy.exe "\\$machine\C$\Users\$OldProfile" "C:\AIP" NTUSER.DAT `/MT:256 | Out-Null
+    $NTUserImp2 = (Test-Path 'C:\AIP\NTUSER.DAT')
+    If ($NTUserImp2 -eq $False){
+        Write-Warning "Unable to copy NTUSER.DAT, will need to be copied manually."
+        Write-Host 'From an Adimistrator Command window type'
+        Write-Host "Robocopy.exe "\\$machine\C$\Users\$OldProfile" "C:\AIP" NTUSER.DAT" -ForegroundColor Red
+        Write-Host 'then hit enter.' 
+        Pause
+    } Else {}
+} Else {}
+
+reg load HKLM\Temp "C:\AIP\NTUSER.DAT"
+$ImportPth = (Test-Path "HKLM:\Temp")
+If ( $ImportPth -eq $False ){
+    Write-Host "Unable to import hive file, trying again."
+    reg load HKLM\Temp "C:\AIP\NTUSER.DAT"
+    $ImportPth2 = (Test-Path "HKLM:\Temp")
+    If ( $ImportPth2 -eq $False ){
+        Write-Warning "Unable to import hive file. Hive file will need to be manually imported."
+        Write-Host 'From an Adimistrator Command window type '
+        Write-Host 'regload HKLM\Temp "C:\AIP\NTUSER.DAT"' -ForegroundColor Red 
+        Write-Host 'then hit enter.' 
+        Pause
+    } Else {}
+} Else {}
+
+# Pulling drives
+Write-Host ""
+Write-Host "Searching for previously mapped share drives" -ForegroundColor Yellow
+$DriveMap = (Get-ItemProperty -Path HKLM:\Temp\Network\* | Select-Object -Property PSChildName, RemotePath)
+ForEach ($DriveMap in $DriveMap) {
+    $DriveStr = [string]$DriveMap
+    $StrLength = [int]$DriveStr.Length
+    $PathLength = [int]( $StrLength - 29)
+    $DriveLtr = ($DriveStr.Substring(14,1))
+    # Write-Host ""
+    # Write-Host "Drive letter = $DriveLtr"
+    $DrivePath = ($DriveStr.Substring(28, ($PathLength)))
+    # Write-Host "Drive path = $DrivePath"
+    Write-Host ""
+    Write-Host " Found network drive $DriveLtr $DrivePath"
+    Add-Content -path $RebuildLog -value "`nFound drive $DriveLtr $DrivePath"
+}
+
+# PST HKEY_LOCAL_MACHINE\Temp\Software\Microsoft\Office\16.0\Outlook\Search
+# https://stackoverflow.com/questions/23800571/registry-key-to-find-pst-locations
+Write-Host ""
+Write-Host "Searching for previously mapped PST files" -ForegroundColor Yellow
+$PstMap = (Get-ItemProperty HKLM:\Temp\Software\Microsoft\Office\16.0\Outlook\Search -Name '*.pst' )
+$PstSplit = ($PstMap -split ';')
+ForEach ($PstSplit in $PstSplit) { 
+    $PstStr = [string] $PstSplit
+    $PstLength = [int]$PstStr.Length
+    $PstTrim = ($PstStr.Substring(1, ($PSTLength - 3)))
+    $PstPath = ($PstTrim -replace "{", "")
+    If  ($PstPath -like '*[.pst]') {
+        # Write-Host ""
+        # Write-Host "Pst path = $PstPath"
+        Write-Host ""
+        Write-Host "Found PST $PstPath"
+        Add-Content -path $RebuildLog -value "`nFound pst $PstPath"
+    } else {}
+}
+
+Copy-Item "C:\AIP\Paths.txt" -Destination "\\$machine\C$\Users\$User\Desktop\Paths.txt"
+
+Write-Host ""
+Write-Host "Open the Mappings.txt file on the user's desktop and remap their shaed drives, pst files" -ForegroundColor Red
+        
+# Data transfer
+# $Source = ("C:\Users\"+$FromPath+"\Contacts"+"").ToString().Insert(0,'"');$Source+='"' $Dest = ("C:\Users"+"\Contacts").ToString().Insert(0,'"');$Dest+='"'
+New-Item "\\$machine\C$\AIP\FileMigration.txt" -ItemType file -Force
+$FromPath = "$OldProfile"
+$Switches = "`/E `/MT:256"
+$FileMigration = "\\$machine\C$\AIP\FileMigration.txt"
+Add-Content $FileMigration -value "@echo off"
+Add-Content $FileMigration -value "cls"
+Add-Content $FileMigration -value "color 17"
+Add-Content $FileMigration -value "Title File Migration"
+Add-Content $FileMigration -value ""
+$Desktop = 'Robocopy "C:\Users\' + $FromPath + '\Desktop" ' + '"C:\Users\' + + '\Desktop" ' + $Switches 
+Add-Content $FileMigration -value $Desktop
+$Favorites = 'Robocopy "C:\Users\' + $FromPath + '\Favorites" ' + '"C:\Users\' + + '\Favorites" ' + $Switches 
+Add-Content $FileMigration -value $Favorites
+$Contacts = 'Robocopy "C:\Users\' + $FromPath + '\Contacts" ' + '"C:\Users\' + + '\Contacts" ' + $Switches 
+Add-Content $FileMigration -value $Contacts
+$Chrome = 'Robocopy "C:\Users\' + $FromPath + '\AppData\Local\Google\Chrome\User Data\Default" ' + '"C:\Users\' + + '\AppData\Local\Google\Chrome\User Data\Default" ' + $Switches 
+Add-Content $FileMigration -value $Chrome
+$FireFox = 'Robocopy "C:\Users\' + $FromPath + '\AppData\Roaming\Mozilla\Firefox\Profiles" ' + '"C:\Users\' + + '\AppData\Roaming\Mozilla\Firefox\Profiles" ' + $Switches 
+Add-Content $FileMigration -value $FireFox
+$Links = 'Robocopy "C:\Users\' + $FromPath + '\Links" ' + '"C:\Users\' + + '\Links" ' + $Switches 
+Add-Content $FileMigration -value $Links
+$Music = 'Robocopy "C:\Users\' + $FromPath + '\Music" ' + '"C:\Users\' + + '\Music" ' + $Switches 
+Add-Content $FileMigration -value $Music
+$OneDrive = 'Robocopy "C:\Users\' + $FromPath + '\OneDrive" ' + '"C:\Users\' + + '\OneDrive" ' + $Switches 
+Add-Content $FileMigration -value $OneDrive
+$Pictures = 'Robocopy "C:\Users\' + $FromPath + '\Pictures" ' + '"C:\Users\' + + '\Pictures" ' + $Switches 
+Add-Content $FileMigration -value $Pictures
+$Videos = 'Robocopy "C:\Users\' + $FromPath + '\Videos" ' + '"C:\Users\' + + '\Videos" ' + $Switches 
+Add-Content $FileMigration -value $Videos
+$Downloads = 'Robocopy "C:\Users\' + $FromPath + '\Downloads" ' + '"C:\Users\' + + '\Downloads" ' + $Switches 
+Add-Content $FileMigration -value $Downloads
+$Documents = 'Robocopy "C:\Users\' + $FromPath + '\Documents" ' + '"C:\Users\' + + '\Documents" ' + $Switches 
+Add-Content $FileMigration -value $Documents
+Add-Content $FileMigration -value 'Pause'
+Add-Content $FileMigration -value 'Verify data has transfered'
+Add-Content $FileMigration -value 'DEL "%~f0"'
+Move-Item -Path "\\$machine\C$\AIP\FileMigration.txt" -Destination "\\$machine\C$\Users\Desktop\$User\FileMigration.bat" -Force
+
+Write-Host ""
+Write-Warning "Ask the user to run the FileMigration.bat file on their desktop to migrate their files"
+
+#Removing temporary files
+Write-Host ""
+Write-Host "Cleaning up" -ForegroundColor Yellow
+Test-Path 'HKCU:\Console\%SystemRoot%_system32_cmd.exe' | Out-Null
+Test-Path 'C:\Windows' | Out-Null
+Start-Sleep -Seconds 30
+Write-Host ""
+Write-Host "Unloading Hive"
+reg unload HKLM\Temp
+$Test12 = (Test-Path "HKLM:\Temp" -PathType Container)
+if ($Test12 -eq $True) {
+    Write-Host ""
+    Write-Host "Hive not removed" -ForegroundColor Red
+ } else {
+    Write-Host ""
+    Write-Host "Hive unloaded" -ForegroundColor Green
+}
+Write-Host ""
+Write-Host "Removing Temporary files"
+Start-Sleep -Seconds 15
+Remove-Item C:\AIP\NTUSER* -Force
+$Test13 = (Test-Path "C:\AIP\NTUSER*")
+If ($Test13 -eq $True) {
+    Write-Host ""
+    Write-Host "Temporary files not removed" -ForegroundColor Red
+} else {
+    Write-Host ""
+    Write-Host "Temporary files removed" -ForegroundColor Green
+}
+
+Remove-Item "C:\AIP\Paths.txt"
+
+Write-Host ""
+Write-Host "Migration finished"
+
+$ErrorType = $Error.exception.message 
+Add-Content -Path $ErrorFile $ErrorType
+
+Write-Host ""
+Write-Host "Migration finished"
+Write-Host "Hit enter to return to rebuild script"
+Pause
+Return
+# Return to other script
+
---
+++ ProfileRebuild.ps1
@@ -0,0 +1,362 @@
+# Title: ProfileRebuild.ps1
+# Authors:
+# Steven Barnes
+# Purpose: 
+# Remotely Automatically backup and delete the user's registry key and rename their user folder.
+#
+# Version: 1.00
+
+<#
+    .Synopsis
+   This is RAP in powershell. Will rebuild the user's profile
+  
+#>
+param(
+    #[Parameter(Mandatory = $True, Position = 1)]
+    $ScriptDir, $Machine, $User
+)
+
+$Host.UI.RawUI.WindowTitle = "Profile rebuild tool"
+$ErrorActionPreference = 'SilentlyContinue'
+Clear-Host
+If ($Null -eq $User){
+    $RebuildInfo = $ScriptDir + ", " +  $Machine
+} else {$RebuildInfo = $ScriptDir + ", " +  $Machine + ", " + $User}
+Write-Host $RebuildInfo
+If ($Null -eq $User){
+    $User = (Read-Host "What is the User's account name?")
+}
+If ($Null -eq $machine){
+    $machine = (Read-Host "What is the User's machine name?")
+}
+#*****Starting Fix*****
+Write-Host 'Would you like to rebuild the profile for' $User 'on' $machine '?'
+$Rebuild = Read-Host '( y / n )'
+Switch ($Rebuild) {  
+    Y {
+        Write-Host "Rebuilding $User's profile on $machine" -ForegroundColor Magenta
+        Write-Host ""
+
+        # Verify no one is logged in
+        $Global:System.SID = Get-WmiObject -Class win32_computersystem -ComputerName $Global:System.machineInt |
+        Select-Object -ExpandProperty Username |
+        ForEach-Object { ([System.Security.Principal.NTAccount]$_).Translate([System.Security.Principal.SecurityIdentifier]).Value }
+        IF ($null -eq $Global:System.SID) {
+            Write-Host "Verified user is logged off" -ForegroundColor Green
+        }
+        Else {
+            $UserPth = ([System.Security.Principal.SecurityIdentifier]($Global:System.SID)).Translate([System.Security.Principal.NTAccount]).Value
+            $Global:System.User = (($UserPth -Split "\\" )[1])
+            Write-Warning "System shows that " + $Global:System.User + " is logged on. Verify that the user logs off prior to continuing."
+            Pause
+        }
+        Pause
+        $Agent = ($env:USERNAME)
+        $error.clear() 
+
+        $Time = (Get-Date).ToString("HH:mm")
+        $Date = (Get-Date).ToString("MM.dd.yyyy")
+        $LongDate = (Get-Date).ToString("yyyy.MM.dd_HHmm")
+        Set-Variable -name LogName -value "$machine-ProfileRebulld-$LongDate.txt" -Scope Global
+        $ProfileLog = "\\ADIDBO390422\C$\AIP\ProfileLogs\$LogName"
+        Add-Content $ProfileLog -value "Profile rebuld started on $LongDate for $User"
+        Add-Content $ProfileLog "Ran by $Agent"
+
+        # Creating AIP folder if it does not exist 
+        $AIP = (Test-Path "C:\AIP")
+        If ($AIP -eq $True) {
+            Write-Host "AIP folder exists"
+        }
+        Else {
+            New-Item -path "C:\" -Name "AIP" -ItemType Directory
+            Write-Host "AIP folder created"
+        } 
+        $AIP = $Null
+        $AIP = (Test-Path "\\$machine\C$\AIP")
+        If ($AIP -eq $True) {
+            Write-Host "AIP folder exists"
+        }
+        Else {
+            New-Item -path "\\$machine\C$\" -Name "AIP" -ItemType Directory
+            Write-Host "AIP folder created"
+        } 
+
+        # Converting user name to SID
+        $objUser = New-Object System.Security.Principal.NTAccount($user)
+        $SID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+        Write-Host "Resolved user's sid: $SID"
+
+        # Verifying registry key
+        Write-Host "Checking for registry key" -ForegroundColor Yellow
+        $reg1 = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $machine)
+        $Test1 = $reg1.OpenSubKey("SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$SID")
+        If ($null -eq $Test1) {
+            Write-Host ""
+            Write-Host "Registry key not found for $user" -ForegroundColor Red
+            Write-Host "Will move on to renaming the local folder"
+            Add-Content $ProfileLog -value "Registry key not found"
+            Pause
+        }
+        Else {
+            # Backing up Key to account's desktop folder
+            Copy-Item $ProfileLog -Destination "\\$machine\C$\AIP\$LogName" -force
+            Write-Host ""
+            Write-Host "Backing up registry key to profile's desktop" -ForegroundColor Yellow
+            Invoke-Command -ComputerName $machine -ArgumentList $SID, $user, $LogName {
+                Param ( $SID, $User, $LogName )
+                Reg export "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$SID" "C:\Users\$User\Desktop\OldKey.reg" /y
+                $Test2 = (Test-Path -path "C:\Users\$User\Desktop\OldKey.reg")
+                If ($Test2 -eq $True) {
+                    Write-Host ""
+                    Write-Host "Key was Backed up. Removing registry key" -ForegroundColor Green
+                    Add-Content "C:\AIP\$LogName" -value "`r`nRegistry key was backed up"
+                    Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$SID"
+                    Write-Host "Checking for registry key" -ForegroundColor Yellow
+                    $reg3 = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $machine)
+                    $Test3 = $reg3.OpenSubKey("SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$SID")
+                    If ($Null -ne $Test3) {
+                        Write-Host ""
+                        Write-Host "Failed to remove registry key" -ForegroundColor Red
+                        Pause
+                    }
+                    Else {
+                        Write-Host ""
+                        Write-Host "Registry key was removed" -ForegroundColor Green
+                        Add-Content "C:\AIP\$LogName" -value "`r`nProfile key was not deleted"
+                    }
+                }
+                else {
+                    Write-Host ""
+                    Write-Host "Failed to back up key" -ForegroundColor Red
+                    Add-Content "C:\AIP\$LogName" -value "`r`nProfile key was not backed up"
+                    Pause
+                    # Renaming registry key
+                    Write-Host ""
+                    Write-Host "Renaming registry key" -ForegroundColor Yellow
+                    $Date = (Get-Date).ToString("MM.dd.yyyy")
+                    $RenamedSID = "Old." + $SID + "." + $Date
+                    Rename-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$SID" -NewName "$RenamedSID"
+                    Write-Host ""
+                    Write-Host "Checking key" 
+                    Write-Host "Checking for registry key" -ForegroundColor Yellow
+                    $reg4 = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $machine)
+                    $Test4 = $reg4.OpenSubKey("SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$SID")
+                    If ($Null -ne $Test4) {
+                        Write-Host ""
+                        Write-Host "Key not renamed" -ForegroundColor Red
+                        Write-Host "You will need to manually rename the key prior to the user logging on" -ForegroundColor Red
+                        Add-Content "C:\AIP\$LogName" -value "`r`nProfile key was not renamed"
+                        Pause
+                    }
+                    else {
+                        Write-Host ""
+                        Write-Host "Key renamed"-ForegroundColor Green
+                        Write-Host ""
+                        Write-Host "New key name:"
+                        Write-Host "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$RenamedSID"
+                        Add-Content "C:\AIP\$LogName" -value "`r`nProfile key was renamed to $RenamedSID"
+                    }
+                }
+            }
+        }
+        Copy-Item "\\$machine\C$\AIP\$LogName" -Destination "\\ADIDBO390422\C$\AIP\ProfileLogs\$LogName" -force
+
+        #Renaming profile
+        Write-Host ""
+        Write-Host "Renaming the Local Profile" -ForegroundColor Yellow
+        Rename-Item -Path "\\$machine\C$\Users\$user" -NewName "old.$user.$Date"
+        $Test5 = (Test-Path "\\$machine\C$\Users\$user" -PathType Container)
+        If ($Test5 -eq $True) {
+            Write-Host ""
+            Write-Host "Unable to rename folder '\\$machine\C$\Users\old.$user.$Date'" -ForegroundColor Red
+            Add-Content $ProfileLog -value "Unable to rename folder, restarting"
+            Write-Host ""
+            write-Host "Restarting computer to see if it can be renamed after restart"
+            Write-Host "Hit Enter to start restart"
+            Write-Host ""
+            Write-Host "Instruct user NOT to log on after the restart" -ForegroundColor Red
+            Pause
+            Restart-Computer -ComputerName $machine -Wait -For PowerShell -Timeout 900 -Delay 2
+            $Service = Get-Service 'ActivID Shared Store Service' -ComputerName $machine
+            if ($service.Status -eq 'Running') {
+                write-Host 'System is back up, attempting to rename the folder again' -ForegroundColor Green
+                Rename-Item -Path "\\$machine\C$\Users\$user" -NewName "old.$user.$Date"
+                $Test6 = (Test-Path "\\$machine\C$\Users\$user" -PathType Container)
+                If ($Test6 -eq $True) {
+                    Write-Host ""
+                    Write-Host "Unable to rename folder '\\$machine\C$\Users\old.$user.$Date'" -ForegroundColor Red
+                    Write-Host "Connect with UNC and attempt to manually rename the folder" -ForegroundColor Red
+                    Add-Content $ProfileLog -value "Unable to rename folder, after the restart"
+                    Pause
+                }
+                else {
+                    Write-Host ""
+                    Write-Host "Folder has been renamed" -ForegroundColor Green
+                    Add-Content $ProfileLog -value "Folder was renamed" 
+                }
+            }
+            else { }
+        }
+        Else {
+            Write-Host ""
+            Write-Host "Folder has been renamed" -ForegroundColor Green
+            Add-Content $ProfileLog -value "Folder was renamed"
+        }
+
+        Add-Content $ProfileLog -value "Verifying issue is resolved"
+        Write-Host "Have user log on and test for the error"
+        Pause
+        Write-host "Is the error resolved?" -ForegroundColor Yellow 
+        $Readhost = Read-Host " ( y / n ) " 
+        Switch ($ReadHost) { 
+    
+            Y {
+
+                # Migration tool call
+                
+                Add-Content $ProfileLog -value "Issue is resolved"
+                Write-Host "Hit enter to start migration"
+                Pause
+
+                $ArgsMen = $ScriptDir + "\MigrationTool.ps1 -Machine " + $Machine + " -User " + $User + " -Date " + $Date + " -LongDate " + $LongDate + " -ProfileLog " + $ProfileLog
+                Start-Process PowerShell.exe -ArgumentList $ARGSMEN -Verbose
+        
+                Pause
+                Write-Host "Completed, Have user verify the data is migrated"
+                Write-Host "Hit Enter to exit"
+                Pause
+                Exit
+            }   
+            N {
+                Write-Host "Hit enter to restore the user's old profile"
+                Pause
+                Write-host "Restoring profile" 
+             
+                # ****************Starting rebuild of old profile***********************
+                Write-Host "Have the user log off" -ForegroundColor Green
+                Write-Host "When the user is at the logon screen hit Enter"
+                Pause
+
+                Add-Content $ProfileLog -value "******Restoring old profile******"
+                # Removing new profile key
+                Write-Host ""
+                Write-Host "Removing new profile registry key" -ForegroundColor Yellow
+                Invoke-Command -ComputerName $machine -ArgumentList $SID, $ProfileLog {
+                    Param ($SID, $ProfileLog)
+                    Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$SID"
+                    $reg7 = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $machine)
+                    $Test7 = $reg7.OpenSubKey("SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$SID")
+                    If ($Null -ne $Test7) {
+                        Write-Host ""
+                        Write-Host "Failed to remove registry key" -ForegroundColor Red
+                        Write-Host "Key will need to be removed manually" -ForegroundColor Red
+                        Add-Content $ProfileLog -value "Key not deleted"
+                        Pause
+                    }
+                    Else {
+                        Write-Host ""
+                        Write-Host "Registry key was removed" -ForegroundColor Green
+                        Add-Content $ProfileLog -value "Profile key was deleted"
+                    }
+                }   
+
+                # Restoring old profile key
+                Write-Host ""
+                Write-Host "Restoring profile registry key" -ForegroundColor Yellow
+                Invoke-Command -ComputerName $machine -ArgumentList $SID, $user, $ProfileLog, $Date {
+                    Param ( $SID, $user, $ProfileLog, $Date)
+                    Reg import "C:\Users\old.$User.$Date\Desktop\OldKey.reg"
+                    $reg8 = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $machine)
+                    $Test8 = $reg8.OpenSubKey("SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$SID")
+                    If ($Null -ne $Test8) {
+                        Write-Host ""
+                        Write-Host "Key was restored" -ForegroundColor Green
+                        Add-Content $ProfileLog -value "Profile key was restored"
+                    }
+                    else {
+                        Write-Host ""
+                        Write-Host "Failed to restore key" -ForegroundColor Red
+                        Write-Host "The key will need to be manually restored from C:\Users\old.$User.$Date"
+                        Add-Content $ProfileLog -value "Profile key was not restored"
+                        Pause
+                    }
+                }
+
+                # Renaming new profile folder
+                Write-Host ""
+                Write-Host "Renaming the Local Profile" -ForegroundColor Yellow
+                Rename-Item -Path "\\$machine\C$\Users\$user" -NewName "old.$user.$Date-1"
+                $Test9 = (Test-Path "\\$machine\C$\Users\$user" -PathType Container)
+                If ($Test9 -eq $False) {
+                    Write-Host ""
+                    Write-Host "Unable to rename folder "\\$machine\C$\Users\$user"" -ForegroundColor Red
+                    Add-Content $ProfileLog -value "Unable to rename folder, restarting"
+                    Write-Host ""
+                    write-Host "Restarting computer to see if it can be renamed after restart"
+                    Write-Host ""
+                    Write-Host "Instruct user NOT to log on after the restart" -ForegroundColor Red
+                    Pause
+                    Restart-Computer -ComputerName Server01 -Wait -For PowerShell -Timeout 900 -Delay 2
+                    $Service = Get-Service 'ActivID Shared Store Service' -ComputerName $machine
+                    if ($service.Status -eq 'Running') {
+                        write-Host 'System is back up, attempting to rename folder again' -ForegroundColor Green
+                        Rename-Item -Path "\\$machine\C$\Users\$user" -NewName "old.$user.$Date-1"
+                        $Test10 = (Test-Path "\\$machine\C$\Users\$user" -PathType Container)
+                        If ($Test10 -eq $False) {
+                            Write-Host ""
+                            Write-Host "Unable to rename folder "\\$machine\C$\Users\$user"" -ForegroundColor Red
+                            Write-Host "Connect with UNC and attempt to manually rename the folder" -ForegroundColor Red
+                            Add-Content $ProfileLog -value "Unable to rename folder"
+                            Pause
+                        }
+                        else {
+                            Write-Host ""
+                            Write-Host "Folder has been renamed" -ForegroundColor Green
+                            Add-Content $ProfileLog -value "Folder was renamed" 
+                        }
+                    }
+                    else { }
+                }
+                Else {
+                    Write-Host ""
+                    Write-Host "Folder has been renamed" -ForegroundColor Green
+                    Add-Content $ProfileLog -value "Folder was renamed"
+                }
+
+                # Renaming old profile folder
+                Write-Host ""
+                Write-Host "Renaming the Local Profile" -ForegroundColor Yellow
+                Rename-Item -Path "\\$machine\C$\Users\old.$user.$Date" -NewName "$user"
+                $Test11 = (Test-Path "\\$machine\C$\Users\old.$user.$Date" -PathType Container)
+                If ($Test11 -eq $False) {
+                    Write-Host ""
+                    Write-Host "Unable to rename folder \\$machine\C$\Users\old.$user.$Date" -ForegroundColor Red
+                    Write-Host "The folder will need to be manually renamed"
+                    Add-Content $ProfileLog -value "Unable to rename folder"
+                    Pause
+                }
+                else {
+                    Write-Host ""
+                    Write-Host "Folder has been renamed" -ForegroundColor Green
+                    Add-Content $ProfileLog -value "Folder was renamed"
+                }
+                Write-Host "Have the user log on and verify the profile has been restored" -ForegroundColor Yellow
+            }    
+        }
+
+        Remove-Variable LogName
+        $ErrorType = $Error.exception.message 
+        Add-Content -Path $ErrorFile $ErrorType
+
+        Write-Host "Finished"
+        Add-Content $ProfileLog -value "Finished at $Time"
+        Pause
+    }
+    N {
+        Write-Host 'Exiting script'
+        Pause
+        Exit
+    }
+
+}
+
